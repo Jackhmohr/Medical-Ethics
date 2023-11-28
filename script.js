258d2cc5a3f53ed74562335ee470a5c028a9b483
Jack Mohr <Jackhmohr@gmail.com>
2023-11-27 20:45:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,51 @@
-// Add an event listener to the form submission
-document.getElementById('surveyForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+// Define the philosophers_responses data
+var philosophers_responses = {
+    "Plato": [3, 2, 1],
+    "Aristotle": [2, 3, 1],
+    "Hegel": [1, 1, 3],
+    "Nietzsche": [1, 3, 3],
+    "Wittgenstein": [1, 2, 3],
+    "Russell": [1, 2, 2]
+};
 
-    // Here you can handle the form data. For example:
-    var formData = new FormData(event.target);
-    var answers = {};
-
-    for (var pair of formData.entries()) {
-        // Use the name attribute to distinguish between questions
-        var questionName = pair[0];
-        var answerValue = pair[1];
-
-        // Store answers in an object using question names as keys
-        answers[questionName] = answerValue;
-    }
-
-    // Calculate the matched philosopher
-    var matchedPhilosopher = calculateMatch(answers);
-
-    // Display the matched philosopher
-    displayMatchedPhilosopher(matchedPhilosopher);
-});
-
-// Define the function to calculate the matched philosopher
+// Function to calculate the matched philosopher
 function calculateMatch(answers) {
-    // Extract response values as integers
-    var responseValues = [];
-    for (var i = 1; i <= 6; i++) {  // Adjust the loop for the number of statements
-        var questionName = 'statement' + i;
-        var answerValue = parseInt(answers[questionName]);
-        responseValues.push(answerValue);
-    }
-
-    // Define the philosophers and their responses
-    var philosophers = {
-        "Plato": [3, 2, 1,5,3,1],
-        "Aristotle": [2, 3, 1,4,6,5],
-        "Hegel": [1, 1, 3,3,2,1],
-        "Nietzsche": [1, 3, 3,6,5,3],
-        "Wittgenstein": [1, 2, 3,3,4,5],
-        "Russell": [1, 2, 2,4,3,1]
-    };
-
-    // Calculate similarity scores for each philosopher
-    var similarityScores = {};
-    for (var philosopher in philosophers) {
-        var philosopherResponses = philosophers[philosopher];
-        var score = 0;
-        for (var i = 0; i < responseValues.length; i++) {
-            score += responseValues[i] * philosopherResponses[i];
+    // Calculate the total score for each philosopher based on user's answers
+    var scores = {};
+    for (var philosopher in philosophers_responses) {
+        var responseValues = philosophers_responses[philosopher];
+        var totalScore = 0;
+        for (var i = 1; i <= 6; i++) {
+            var answerValue = parseInt(answers['statement' + i]);
+            totalScore += Math.abs(answerValue - responseValues[i - 1]);
         }
-        similarityScores[philosopher] = score;
+        scores[philosopher] = totalScore;
     }
 
-    // Find the philosopher with the highest similarity score
-    var matchedPhilosopher = Object.keys(similarityScores).reduce(function (a, b) {
-        return similarityScores[a] > similarityScores[b] ? a : b;
+    // Find the philosopher with the minimum total score (closest match)
+    var matchedPhilosopher = Object.keys(scores).reduce(function (a, b) {
+        return scores[a] < scores[b] ? a : b;
     });
 
     return matchedPhilosopher;
 }
 
-// Display the matched philosopher on the page
+// Function to display the matched philosopher on the page
 function displayMatchedPhilosopher(matchedPhilosopher) {
     var resultDiv = document.getElementById('result');
     var matchedPhilosopherElement = document.getElementById('matchedPhilosopher');
 
     matchedPhilosopherElement.textContent = matchedPhilosopher;
     resultDiv.classList.remove('hidden');
+}
+
+// Function to handle form submission and calculate/display the match
+function calculateMatchAndDisplay() {
+    var formData = new FormData(document.getElementById('surveyForm'));
+    var answers = {};
+    for (var pair of formData.entries()) {
+        answers[pair[0]] = pair[1];
+    }
+    var matchedPhilosopher = calculateMatch(answers);
+    displayMatchedPhilosopher(matchedPhilosopher);
 }
