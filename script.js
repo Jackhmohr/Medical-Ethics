778544e52d301cbfdb7317b94738710cac48e0e8
Jack Mohr <Jackhmohr@gmail.com>
2023-11-27 22:03:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Define the philosopher data
 var philosophers = {
-    "Plato": [3, 2, 1, 5, 3, 1],
+    "Kant": [3, 2, 1, 5, 3, 1],
     "Aristotle": [2, 3, 1, 4, 6, 5],
-    "Hegel": [1, 1, 3, 3, 2, 1],
+    "John Stuart Mill": [1, 1, 3, 3, 2, 1],
     "Nietzsche": [1, 3, 3, 6, 5, 3],
-    "Wittgenstein": [1, 2, 3, 3, 4, 5],
-    "Russell": [1, 2, 2, 4, 3, 1]
+    "John Rawls": [1, 2, 3, 3, 4, 5],
+    "Philippa Foot": [1, 2, 2, 4, 3, 1]
 };
 
 // Function to calculate the matched philosopher
